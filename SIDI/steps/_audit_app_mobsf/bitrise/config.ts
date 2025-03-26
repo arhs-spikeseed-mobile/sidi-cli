77a9effb09a799e3f5236a05d677415c14aa7644
Imran Mnts <imran.mentese@gmail.com>
2025-03-26 14:02:40 +0100

    SIDI-0000 - run prettier

--- SIDI/steps/_audit_app_mobsf/bitrise/config.ts
+++ SIDI/steps/_audit_app_mobsf/bitrise/config.ts
@@ -1,7 +1,10 @@
 import { IConfig } from '../../../../src/models/SidiModel';
 
 const config: IConfig = {
-  mandatoryKeys: [{ key: 'MOBSF_CHECK_PATH', defaultValue: "." }, { key: 'MOBSF_THRESHOLD', defaultValue: "WARNING" }],
+  mandatoryKeys: [
+    { key: 'MOBSF_CHECK_PATH', defaultValue: '.' },
+    { key: 'MOBSF_THRESHOLD', defaultValue: 'WARNING' },
+  ],
 };
 
 export default config;
